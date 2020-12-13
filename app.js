906a43a8c7a5ce0b1e0cb3e972cb2f4e2ed6ab78
portia-cyber <mop10@icloud.c.om>
2020-12-13 09:32:58 +0100

    update

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
 
 //middleware
-app.use('/posts',()=>{
-    console.log('this is a middleware running')
-})
+//app.use('/posts',()=>{
+//  console.log('this is a middleware running')
+//})
+
+//connect to db
+mongoose.connect('mongodb+srv://admin:admin@rest.exrnc.mongodb.net/<dbname>?retryWrites=true&w=majority',
+ { useNewUrlParser: true }, () => console.log('connected to DB'))
+
 
 //routes 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('we are home')
 })
 
 //post example
-app.get('/posts',(req, res)=>{
+app.get('/posts', (req, res) => {
     res.send('we are on post')
 })
 
