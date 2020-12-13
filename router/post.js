c14d55dc0d13f69e322b01828f191f56b645c448
portia-cyber <mop10@icloud.c.om>
2020-12-13 16:45:50 +0100

    Update

--- router/post.js
+++ router/post.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router =express.Router();
+const Post = require('../models/Post');
 
 //post example
 router.get('/', (req, res) => {
     res.send('we are on post')
 })
 
+ 
+
+router.post('/',(req, res)=>{
+    console.log(req.body)
+})
 module.exports = router;
