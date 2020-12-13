f80c809e3534180f008212b4cc84686c44aeefe1
portia-cyber <mop10@icloud.c.om>
2020-12-13 18:17:13 +0100

    Update post.js

--- router/post.js
+++ router/post.js
@@ -3,12 +3,26 @@ const router = express.Router();
 const Post = require('../models/Post');
 
 //post example
-router.get('/', (req, res) => {
-    res.send('we are on post')
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.json(posts)
+    } catch (err) {
+        res.json({ message: err })
+    }
+
 })
 
 router.get('/specific', (req, res) => {
     res.send('specific post')
+})
+router.get('/', async (req, res) => {
+    try {
+        const posts = await post.find()
+        res.json(posts)
+    } catch (err) {
+        res.json({ message: err })
+    }
 })
 
 router.post('/', async (req, res) => {
@@ -22,8 +36,50 @@ router.post('/', async (req, res) => {
 
         const savePost = await post.save()
         res.json(savePost);
-    }catch(err){
-        res.json({message: err})
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
+
+//get specific post 
+
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(res.params.postId)
+        res.json(post)
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
+
+//delete post 
+
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removePost = await Post.remove({ _id: res.params.postId })
+        res.json(removePost)
+    } catch (err) {
+        res.json({ message: err })
+    }
+})
+
+//update post 
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatePost = await Post.updateOne(
+            {
+                _id: req.params.postId
+            },
+            {
+                $set: { title: req.body.title }
+            }
+        )
+        res.json(updatePost)
+    } catch (err) {
+        res.json({
+            message: err
+        })
     }
 })
 module.exports = router;
+
