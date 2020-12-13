87521f7f3ee6e087a7261dfc98c9b4e1c5b66c4a
portia-cyber <mop10@icloud.c.om>
2020-12-13 09:03:45 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 
-//routes 
+//middleware
+app.use('/posts',()=>{
+    console.log('this is a middleware running')
+})
 
+//routes 
+app.get('/', (req, res)=>{
+    res.send('we are home')
+})
+
+//post example
+app.get('/posts',(req, res)=>{
+    res.send('we are on post')
+})
 
 //server
 app.listen(3000);
